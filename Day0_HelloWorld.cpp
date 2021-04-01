28892e6afb5a304a4ce1e098b12426c243c8b883
Arsh Mishra <57450577+arshmishra007@users.noreply.github.com>
2021-04-01 15:12:32 +0530

    Create Day0_HelloWorld.cpp

---
+++ Day0_HelloWorld.cpp
@@ -0,0 +1,19 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main()
+{
+    cout<<"Hello, World."<<endl;
+    char b[10000];
+    cin.getline(b,10000);
+    cout<<b;
+    
+    return 0;
+    
+    
+}
